c951bf660c96c258d1fa98e89a685990261c4dab
May Mu Khaing <maymukhaing1993@gmail.com>
2022-03-11 17:44:33 +0630

    Created seeds file for smaple data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+10.times do |blog|
+  Blog.creat!(
+  	 title: "My Blog Post #{blog}",
+  	 body: "dbfbeifbeifbeisbvjsdvbjjvbsdbvdjvbjd
+  	 vbjsdbvjsdbvjdbvdjvbjdvdvvdvdvdvdvfdvdfvdfvd"
+  	)
+end
+
+puts "10 blog posts created"
+
+5.times do |skill|
+  Skill.creat!(
+  	 title: "Rails #{skill}",
+  	 percent_utilized: 15
+  	)
+end
+
+puts "5 skills created"
+
+9.times do |portfolio_item|
+	Portfolio.create!(
+		title: "Portfolio title: #{portfolio_item}",
+		subtitle: "My great service",
+		body: "jfsdfbsfbdsjfbsjfbsdjbsdbjsbbjb
+		  fkfnskfnskdnfsnfsfnslfnsdnf",
+		main_image: "http://placehold.it/350*200",
+		thumb_image: "http://placehold.it/350*200"
+		)
+end
+
+puts "9 portfolio items created"
